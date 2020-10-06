37b82b659c7e21a0945e5423258222c504f3dc7e
Dennis <Dennis@192.168.1.135>
2020-10-06 03:27:39 +0200

    Funcion Copiar y leer archivos

--- GestionArchivos/src/GestionArchivos/CopyFile.java
+++ GestionArchivos/src/GestionArchivos/CopyFile.java
@@ -1,5 +1,120 @@
 package GestionArchivos;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CopyFile {
+	
+	String ficheroacopiar;
+	public CopyFile(String rutafichero ,String nombrefichero) throws UnsupportedEncodingException {
+		
+		CrearFichero(rutafichero,nombrefichero);
+		ficheroacopiar=nombrefichero;
+		
+	}
+	
+	public void LeerFichero(String ficheroectura) {
+		
+	
+
+		
+	}
+	public void CrearFichero(String rutafichero,String nombrefichero) throws UnsupportedEncodingException {
+		
+		String cadena=null;
+		FileReader f=null;
+		File fichero=null;
+		ArrayList<String>texto=new ArrayList();
+		
+		try
+		{
+		f= new FileReader(nombrefichero);
+		BufferedReader b = new BufferedReader(f);
+		fichero= new File(rutafichero,"ficherocopiado.txt");
+		
+		try {
+			  // A partir del objeto File creamos el fichero físicamente
+			  if (fichero.createNewFile())
+			    System.out.println("Fichero copiado correctamente ");
+			  else
+			    System.out.println("No ha podido ser creado el fichero");
+			} catch (IOException ioe) {
+			  ioe.printStackTrace();
+			}
+		while((cadena = b.readLine())!=null)
+		{
+			texto.add(cadena);
+		}
+		b.close();
+		}catch( FileNotFoundException e)
+		{
+		System.out.println("Fichero "+nombrefichero+" no encontrado");
+		}catch( IOException e)
+		{
+		System.out.println("IOException :o");
+		}
+		
+		Writer out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fichero),"UTF-8"));
+			
+			// Escribimos linea a linea en el fichero
+			
+				try {
+					for(int i=0;i<texto.size();i++) {
+							
+						out.write(texto.get(i)+"\n");
+						
+					}
+				
+					
+				} catch (IOException ex) {
+					System.out.println("Mensaje excepcion escritura: " + ex.getMessage());
+				}
+
+		} catch (FileNotFoundException ex2) {
+			System.out.println("Mensaje error 2: " + ex2.getMessage());
+		} finally {
+			try {
+				out.close();
+			} catch (IOException ex3) {
+				System.out.println("Mensaje error cierre fichero: " + ex3.getMessage());
+			}
+		}
+
+		
+	
+
+
+  }
+		
+		
+		
+		
+
+	
+	public static void main(String args[]) throws UnsupportedEncodingException {
+		
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("introduzca el nombre del fichero que desea copiar");
+		String nombre=teclado.nextLine();
+		System.out.println("introduzca la ruta donde desea copiarlor");
+		String ruta =teclado.nextLine();
+		
+		
+		CopyFile archivocopiado= new CopyFile(ruta,nombre);
+		
+	}
+	
 
 }
--- GestionArchivos/src/GestionArchivos/ReadFile.java
+++ GestionArchivos/src/GestionArchivos/ReadFile.java
@@ -1,5 +1,33 @@
 package GestionArchivos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadFile {
+	
+	
+	public ReadFile(String fichero) throws FileNotFoundException {
+		
+		String cadena=null;
+		FileReader ficherolectura= new FileReader(fichero);
+		BufferedReader buffer= new BufferedReader(ficherolectura);
+		
+		try {
+			while((cadena = buffer.readLine())!=null) {
+				
+				System.out.println(cadena);
+			}
+		}catch( FileNotFoundException e)
+		{
+		System.out.println("Fichero "+fichero+" no encontrado");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
