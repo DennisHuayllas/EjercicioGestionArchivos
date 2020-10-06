73e5fb74f8640b9cfa3b45e97782e03354c185e7
Luigi <Luigi@DESKTOP-3F0O9KA>
2020-10-06 12:35:48 +0200

    Clase crear ficheros

---
+++ GestionArchivos/src/GestionArchivos/CreateFile.java
@@ -0,0 +1,34 @@
+package GestionArchivos;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+
+
+    File fichero ;
+
+    protected CreateFile() {
+
+        fichero = null;
+
+    }
+
+     protected CreateFile(String nruta,String nfichero) {
+
+
+        fichero = new File (nruta,nfichero);
+
+         try {
+
+              if (fichero.createNewFile())
+                System.out.println("El fichero se ha creado correctamente");
+              else
+                System.out.println("No ha podido ser creado el fichero");
+            } catch (IOException ioe) {
+              ioe.printStackTrace();
+            }
+
+         }
+
+}
